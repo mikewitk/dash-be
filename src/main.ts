0f71cdf156af693ad7cc4c1a7a7d5db04caa1d5b
MIke Witk <mike.witk@gmail.com>
2022-12-04 19:25:08 -0500

    chore: saving user to db

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      // make sure incoming requests do not have extra properties.
+      // Any additional properties are removed
+      whitelist: true,
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
